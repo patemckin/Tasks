d9bc0e2784852e5728b84d483838d618de883558
patemckin <takiprosit@gmail.com>
2015-10-03 10:44:16 +0300

    Next task

---
+++ Task2/src/main/java/Point.java
@@ -0,0 +1,24 @@
+/**
+ * Created by admin on 03/10/15.
+ */
+public class Point {
+    private static int x,y;
+
+    public static int getX(){
+        return x;
+    }
+
+    public static int getY(){
+        return y;
+    }
+
+    public static void setX(int _x){
+        x = _x;
+    }
+
+    public static void setY(int _y){
+        y = _y;
+    }
+}
+
+
---
+++ Task2/src/main/java/Sort.java
@@ -0,0 +1,40 @@
+import static java.lang.Math.*;
+import static java.lang.System.out;
+
+
+public class Sort {
+    double compare(Point a, Point b){
+        return sqrt(((Point.getX())^2 + (Point.getY())^2));
+    }
+
+    public static void main(String[] args) {
+        double a[] = parseString(args);
+        sort(a);
+        printArray(a);
+    }
+
+    private static double[] parseString(String[] input) {
+        double result[] = new double[input.length];
+        for (int i = 0; i < input.length; i++) {
+            result[i] = Double.parseDouble(input[i]);
+        }
+        return result;
+    }
+
+    public static boolean sort(double arr[]) {
+        for (int j = 1; j < arr.length; ++j) {
+            double key = arr[j];
+            int i = j - 1;
+            while (i >= 0 && arr[i] > key)
+                arr[i + 1] = arr[i--];
+            arr[i + 1] = key;
+        }
+        return true;
+    }
+
+    public static void printArray(double arr[]) {
+        for (int i = 0; i < arr.length; ++i)
+            out.printf("%g ", arr[i]);
+        out.println();
+    }
+}
